77ba5236dcf9cf566055ef5c55d2377dad05749c
ykrueng <yasirah.krueng@gmail.com>
2018-12-16 20:52:42 -0600

    add project page

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page',
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to fulfill this request.'
